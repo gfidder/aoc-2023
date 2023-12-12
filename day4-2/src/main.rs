7dbf0caa70352c5173d7f0db7c1eec4c07fa59a5
gfidder <gabfid@gmail.com>
2023-12-12 23:08:07 +0000

    updating for puzzle

--- day4-2/src/main.rs
+++ day4-2/src/main.rs
@@ -1,19 +1,22 @@
-use std::cmp::max;
-use std::fs::read_to_string;
+use std::{borrow::BorrowMut, cell::RefCell, collections::HashMap, fs::read_to_string, rc::Rc};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Game {
     winning_numbers: Vec<u32>,
     played_numbers: Vec<u32>,
     matches: u32,
+    game_number: u32,
+    copies: u32,
 }
 
 impl Game {
-    pub fn new(winning_numbers: Vec<u32>, played_numbers: Vec<u32>) -> Self {
+    pub fn new(winning_numbers: Vec<u32>, played_numbers: Vec<u32>, game_number: u32) -> Self {
         Self {
             winning_numbers,
             played_numbers,
             matches: 0,
+            game_number,
+            copies: 1,
         }
     }
 
@@ -36,31 +39,82 @@ impl Game {
 
         0
     }
+
+    pub fn get_game_copies(&self) -> Vec<u32> {
+        let mut game_copies = Vec::new();
+        let base_game = self.game_number;
+
+        for i in 1..=self.matches {
+            let copied_game = base_game + i;
+            game_copies.push(copied_game);
+        }
+
+        game_copies
+    }
+
+    pub fn add_copy(&mut self) {
+        self.copies += 1;
+    }
+
+    pub fn get_num_copies(&self) -> u32 {
+        self.copies
+    }
 }
 
 fn main() {
-    let mut all_games = Vec::<Game>::new();
+    let mut all_games = Vec::new();
     let game_inputs = read_lines();
     let mut sum: u32 = 0;
+    let mut i = 0;
+    let mut game_copies = HashMap::new();
 
     for input in &game_inputs {
-        all_games.push(process_game(input));
-        let mut game = process_game(input);
+        let game_number = i + 1;
+        let mut game = process_game(input, game_number);
+
         game.calculate_matches();
 
-        sum += game.get_game_total();
+        all_games.push(game);
+
+        i += 1;
+    }
+
+    for i in 1..=all_games.len() {
+        game_copies.insert(i, 1usize);
+    }
+
+    for game in all_games.iter() {
+        let things = game.get_game_copies();
+
+        for i in things {
+            match game_copies.get_mut(&(i as usize)) {
+                Some(new_game) => {
+                    *new_game += 1;
+                }
+                None => {
+                    panic!("Found game that doesn't exist: {}!", i);
+                }
+            }
+        }
+    }
+
+    println!("{:?}", game_copies);
+
+    for game in all_games.iter() {
+        println!("{:?}", game);
+        sum += game.get_num_copies();
     }
 
     println!("{}", sum);
 }
 
-fn process_game(input: &String) -> Game {
+fn process_game(input: &String, game_number: u32) -> Game {
     let x: Vec<&str> = input.split_terminator(&[':', '|'][..]).collect();
 
     let winning_numbers = get_numbers(x[1]);
     let played_numbers = get_numbers(x[2]);
 
-    Game::new(winning_numbers, played_numbers)
+    Game::new(winning_numbers, played_numbers, game_number)
 }
 
 fn get_numbers(input: &str) -> Vec<u32> {
